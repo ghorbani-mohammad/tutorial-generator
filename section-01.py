9865a61a30cfc406bb6b5f059318e8142d27de5b
Mohammad <mohammad.ghorbani@hotmail.com>
2022-12-11 23:36:39 +0330

    add simple generator function

--- section-01.py
+++ section-01.py
@@ -5,3 +5,11 @@ def infinite_sequence():
     while True:
         return num
         num += 1
+
+# Converting above function to a generator
+
+def infinite_sequence():
+    num = 0
+    while True:
+        yield num
+        num += 1
