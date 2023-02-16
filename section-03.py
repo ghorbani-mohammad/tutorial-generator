0e933dd8ed6b2bdb7b56812db12fbdb8f40952cb
Mohammad <mohammad.ghorbani@hotmail.com>
2023-02-16 14:48:20 +0330

    Update comments

--- section-03.py
+++ section-03.py
@@ -1,5 +1,7 @@
 # Generator by comprehension
-# Notice that in Python the structure of (expression) is a generator
+# Notice that in Python, using parenthesis (expression) is a generator
+# Look at below example, squared is a generator
+# Notice that you can use generator in for loops without next
 
 squared = (x**2 for x in range(5))
 for num in squared:
